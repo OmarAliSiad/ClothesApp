6217cf0095e6eb0f61352a85e345aa1e0e34255d
Omar <105920279+OmarAliSiad@users.noreply.github.com>
2024-12-18 18:43:33 +0200

    Update app_styles.dart

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -111,30 +111,3 @@ abstract class AppStyles {
   }
 }
 
-// // sacleFactor
-// // responsive font size
-// // (min , max) fontsize
-// double responsiveFontSize(BuildContext context, {required double fontSize}) {
-//   double scaleFactor = getScaleFactor(context);
-//   double responsiveFontSize = fontSize * scaleFactor;
-
-//   double lowerLimit = fontSize * .8;
-//   double upperLimit = fontSize * 1.2;
-
-//   return responsiveFontSize.clamp(lowerLimit, upperLimit);
-// }
-
-// double getScaleFactor(context) {
-//   // var dispatcher = PlatformDispatcher.instance;
-//   // var physicalWidth = dispatcher.views.first.physicalSize.width;
-//   // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
-//   // double width = physicalWidth / devicePixelRatio;
-//   double width = MediaQuery.sizeOf(context).width;
-//   if (width < SizeConfig.tablet) {
-//     return width / 411.42857142857144; //550
-//   } else if (width < SizeConfig.desktop) {
-//     return width / 1000;
-//   } else {
-//     return width / 1920;
-//   }
-// }
